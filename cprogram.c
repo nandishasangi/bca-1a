f9431a34e746628e4f8b4064b728ae8dc522df03
Nandish Asangi <nandish.asangi@gmail.com>
2024-09-06 10:56:45 +0530

    Added Conio Line

--- cprogram.c
+++ cprogram.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
+#include<conio.h>
 
 main(){
     int x, y, sum;
     clrscr();
     sum = x + y;
-    printf("Sum = %d", sum);
+    printf("Sum of 2 numbers = %d", sum);
     getch();
     return 0;
 }
